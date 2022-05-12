ad6c84583a8f2b416666d9e8898a5dad1766454b
DESKTOP-GR2DPA5\whege <whegedusich@gmail.com>
2022-05-12 00:25:12 -0500

    updated classes so that there is only one instance for each type (except for Hint, which is variable). Added getter and setter methods for Hint class

--- pysweeper/common/assets.py
+++ pysweeper/common/assets.py
@@ -20,26 +20,40 @@ class Asset:
         return self._char
 
 
-class Empty(Asset):
+class _Empty(Asset):
     def __init__(self, space_icon=_unicode_box, /):
-        super().__init__(space_icon)
+        super(_Empty, self).__init__(space_icon)
 
 
-class Flag(Asset):
+class _Flag(Asset):
     def __init__(self, flag_icon=_unicode_flag, /):
-        super().__init__(flag_icon)
+        super(_Flag, self).__init__(flag_icon)
 
 
 class Hint(Asset):
-    def __init__(self, num_neighbors: int):
+    def __init__(self, num_neighbors: int = 0):
         super(Hint, self).__init__(str(num_neighbors))
 
+    @property
+    def count(self):
+        return self._char
 
-class Mine(Asset):
+    @count.setter
+    def count(self, ct, /) -> None:
+        self._char = str(ct)
+
+
+class _Mine(Asset):
     def __init__(self, mine_icon=_unicode_star, /):
-        super().__init__(mine_icon)
+        super(_Mine, self).__init__(mine_icon)
 
 
-class Revealed(Asset):
+class _Revealed(Asset):
     def __init__(self, icon=_unicode_space, /):
-        super().__init__(icon)
+        super(_Revealed, self).__init__(icon)
+
+
+Empty = _Empty()
+Flag = _Flag()
+Mine = _Mine()
+Revealed = _Revealed()
